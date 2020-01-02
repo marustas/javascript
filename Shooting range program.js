58a1ba4e5dd0a4750d1768746ff72ee11d2cb576
Stas <marudov.stas@gmail.com>
2020-01-02 22:56:06 +0300

    Testing program using "promise"

---
+++ Shooting range program.js
@@ -0,0 +1,28 @@
+ let drink = 0;
+ function shoot(arrow){
+     console.log('Вы сделали выстрел');
+     let promise = new Promise(function(resolve,reject){
+         setTimeout(function(){
+Math.random() > .5 ? resolve({}) : reject('Вфы промахнулись');
+         }, 3000);
+     });
+     return promise;
+ };
+  
+ function win() {
+ console.log('Вы победили');
+  (drink == 1) ? buyBeer() : giveMoney();
+ }
+ function buyBeer(){
+     console.log('Вам купили пиво');
+ }
+ function giveMoney(){
+     console.log('Ва заплатили');
+ }
+ function loose(){
+     console.log('Вы проиграли');
+ }
+ shoot({})
+   .then(mark=> console.log('Вы попали в цель'))
+   .then(win) 
+   .catch(loose);         
